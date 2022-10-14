811b991eb9807605c101831af9d68f7d9f07bb1a
ALBANO Daniel <deanidev@gmail.com>
2022-10-14 15:47:40 +0200

    #04-03-001 single news generic view and DOM tree WIP

---
+++ views/news/newsSingle.php
@@ -0,0 +1,52 @@
+<!-- header outside, or inside main -->
+<!-- article can have another main?  -->
+<main>
+    <section class="theNews">
+    
+    <article class="container">
+        <div class="row">
+            <img src="/public/assets/img/wave/1-landscape.jpg" alt="main image of news">
+            <h1>news name</h1>
+            <h3>Meta date,autor,etc</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt odit sint placeat animi iusto pariatur, asperiores porro sunt ipsa magni officiis perspiciatis voluptatem, rem rerum error, aut enim fugiat molestiae!</p>
+            <div class="divider">divider or PUB</div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus corporis temporibus dolorum iste ut? Laudantium, veniam voluptatum iusto excepturi quisquam recusandae commodi eum, beatae voluptas, similique omnis nihil quae maiores.</p>
+        </div>
+        <div class="row ifMoreImgs">
+            <img class="col s12 m6" src="/public/assets/img/wave/3-thumbnail.jpg" alt="generic article img">
+            <p class="col s12 m6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore porro repudiandae veniam laboriosam nisi eligendi delectus aliquam reprehenderit voluptatem earum temporibus esse corrupti iusto, sint quo tenetur. Dolor, doloribus hic!</p>
+        </div>
+        <div class="row ifMoreImgs">
+            <p class="col s12 m6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur nesciunt maiores culpa perspiciatis ut, atque officiis, doloremque ad harum adipisci perferendis voluptatem voluptatibus, nulla accusantium. Dolore ab dolores aperiam id.</p>
+            <img class="col s12 m6" src="/public/assets/img/wave/3-thumbnail.jpg" alt="generic article img">
+        </div>
+        <div class="row ifMoreImgs">
+            <img class="col s12 m6" src="/public/assets/img/wave/3-thumbnail.jpg" alt="generic article img">
+            <p class="col s12 m6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore error aspernatur magnam delectus eum, non cumque fuga quam inventore, doloremque harum ex, vitae illo totam beatae eaque quidem nisi nemo?</p>
+        </div>
+        <div class="row ifMoreImgs">
+            <p class="col s12 m6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nisi perspiciatis nostrum dolor tempore numquam sint aliquid adipisci. A soluta ratione labore voluptatibus exercitationem, saepe excepturi in eos ipsam veniam.</p>
+            <img class="col s12 m6" src="/public/assets/img/wave/3-thumbnail.jpg" alt="generic article img">
+        </div>
+    </article>
+    <div class="container coments">
+        <h3 class="row">comments:</h3>
+        <div class="comment">
+            <img class="col s12 m6" src="/public/assets/img/wave/3-thumbnail.jpg" width="100%" height="50%" alt="profile">
+            <p> i loved this post , really inspiring!</p>
+        </div>
+        <div class="comment">
+            <img class="col s12 m6" src="/public/assets/img/wave/3-thumbnail.jpg" width="100%" height="50%" alt="profile">
+            <p> i loved this post , really inspiring!</p>
+        </div>
+    </div>
+    </section>
+    <section>
+        <div class="moreNews">
+            <p>
+            little followUp navigation suggestion, news or map or features etc....<br> maybe some caroussel with the related news, more news, or other cathegories...
+            </p>
+
+        </div>
+    </section>
+</main>
