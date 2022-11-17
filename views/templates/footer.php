915c75dcabc67f2f9b11c9f4a800dfda41c9e020
ALBANO Daniel <deanidev@gmail.com>
2022-11-17 17:01:07 +0100

    DONE contactus WIP center headers

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -8,10 +8,10 @@
             <div class="col l4 offset-l2 s12">
                 <h5 class="white-text">Links</h5>
                 <ul>
+                    <li><a class="grey-text text-lighten-3" href="/contactus">Contact/Support</a></li>
                     <li><a class="grey-text text-lighten-3" href="#!">About Us</a></li>
                     <li><a class="grey-text text-lighten-3" href="#!">Cookies</a></li>
                     <li><a class="grey-text text-lighten-3" href="#!">Mentions Legales</a></li>
-                    <li><a class="grey-text text-lighten-3" href="contactus"></a>Contact/Support</li>
                 </ul>
             </div>
         </div>
@@ -19,7 +19,7 @@
     <div class="footer-copyright blue-grey darken-4">
         <div class="container">
             © 2022 Tupanetwork
-            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            <!-- <a class="grey-text text-lighten-4 right" href="#!">More Links</a> -->
         </div>
     </div>
 </footer>
