426b89783796008f11af745724b2a8fccacf6ad9
ALBANO Daniel <deanidev@gmail.com>
2022-11-24 14:08:49 +0100

    wip

--- views/home.php
+++ views/home.php
@@ -1,4 +1,5 @@
 <header>
+    <h1 class="display-none">SURF SPOTS PREVISIONS AND SURF NEWS</h1>
     <!-- parallax -->
     <div class="row s12">
         <div class="parallax-container valign-wrapper hide-on-large-only">
@@ -13,155 +14,98 @@
     </div>
     <!-- parallax END -->
 </header>
-<main> 
-<section class="gray lighten-5">
-    <div class="container">
-        <div class="row">
-            <div class="col s12">
-                <h3 class="center-align ">this is h3, inside section..</h3>
-                <blockquote>
-                    This is an example quotation that uses the blockquote tag.
-                </blockquote>
-                <p class="flow-text">this is a paragraph inside section,<br> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis distinctio amet veniam odit consequuntur, repudiandae reiciendis est minima et quam optio esse maiores corrupti rerum voluptates culpa eaque similique alias.</p>
-            </div>
-        </div>
-    </div>
-</section>
-<!-- parallax -->
-<div class="parallax-container hide-on-large-only">
-    <div class="parallax"><img src="/public/assets/img/wave/1-tablet.jpg"></div>
-</div>
-<div class="parallax-container hide-on-med-and-down">
-    <div class="parallax"><img src="/public/assets/img/wave/1-landscape.jpg"></div>
-</div>
-<!-- parallax END -->
-<section class="gray lighten-5">
-    <div class="container">
-        <div class="row">
-            <div id='homeCarousel' class="carousel s12">
-                <h3 class="center">Features:</h3>
-                <div class="carousel-item">
-                    <div class="card hoverable">
-                        <div class="card-image ">
-                            <img src="/public/assets/img/card1.jpg">
-                            <span class="card-title">Card Title</span>
-                            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
-                        </div>
-                        <div class="card-content">
-                            <p>I am a very simple card.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item ">
-                    <div class="card hoverable">
-                        <div class="card-image ">
-                            <img src="/public/assets/img/card2.jpg">
-                            <span class="card-title">Card Title</span>
-                            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
-                        </div>
-                        <div class="card-content">
-                            <p>I am a very simple card.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item ">
-                    <div class="card hoverable">
-                        <div class="card-image ">
-                            <img src="/public/assets/img/card3.jpg">
-                            <span class="card-title">Card Title</span>
-                            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
-                        </div>
-                        <div class="card-content">
-                            <p>I am a very simple card.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item ">
-                    <div class="card hoverable">
-                        <div class="card-image ">
-                            <img src="/public/assets/img/b41aaf23612851ab156054324bc7de30.jpg">
-                            <span class="card-title">Card Title</span>
-                            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
-                        </div>
-                        <div class="card-content">
-                            <p>I am a very simple card.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item ">
-                    <div class="card hoverable">
-                        <div class="card-image ">
-                            <img src="/public/assets/img/b41aaf23612851ab156054324bc7de30.jpg">
-                            <span class="card-title">Card Title</span>
-                            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
-                        </div>
-                        <div class="card-content">
-                            <p>I am a very simple card.</p>
-                        </div>
-                    </div>
+<main>
+    <section id="intro" class="gray lighten-5">
+        <div class="container">
+            <div class="row">
+                <div class="col s12">
+                    <h3 class="center-align ">where are the haoles !?</h3>
+                    <blockquote>
+                        "If you're having a bad day, catch a wave."
+                    </blockquote>
+                    <p class="flow-text">We made this app to make you the respectfull and informed surfer our ocean needs... <br> no more getting hurt by unespected rocks, no more flat oceans, no more feeling alone.. <br> we are a community to share good moments and help each other to get the waves we need. </p>
+                    <p class="flow-text">We made this app to make you the respectfull and informed surfer our ocean needs... <br> no more getting hurt by unespected rocks, no more flat oceans, no more feeling alone.. <br> we are a community to share good moments and help each other to get the waves we need. </p>
+                    <p class="flow-text">We made this app to make you the respectfull and informed surfer our ocean needs... <br> no more getting hurt by unespected rocks, no more flat oceans, no more feeling alone.. <br> we are a community to share good moments and help each other to get the waves we need. </p>
+                    <p class="flow-text">We made this app to make you the respectfull and informed surfer our ocean needs... <br> no more getting hurt by unespected rocks, no more flat oceans, no more feeling alone.. <br> we are a community to share good moments and help each other to get the waves we need. </p>
                 </div>
             </div>
         </div>
+    </section>
+    <!-- parallax -->
+    <div class="parallax-container hide-on-large-only">
+        <div class="parallax"><img src="/public/assets/img/wave/2-landscape.jpg"></div>
     </div>
-</section>
-<!-- parallax -->
-<div class="parallax-container hide-on-large-only">
-    <div class="parallax"><img src="/public/assets/img/wave/1-tablet.jpg"></div>
-</div>
-<div class="parallax-container hide-on-med-and-down">
-    <div class="parallax"><img src="/public/assets/img/wave/1-landscape.jpg"></div>
-</div>
-<!-- parallax END -->
-<section class="gray lighten-5">
-    <div class="container">
-        <section class="row">
-            <div class="col s12">
-                <h3 class="center-align ">this is h3, inside section..</h3>
-                <blockquote>
-                    This is an example quotation that uses the blockquote tag.
-                </blockquote>
-                <p class="flow-text">this is a paragraph inside section,<br> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis distinctio amet veniam odit consequuntur, repudiandae reiciendis est minima et quam optio esse maiores corrupti rerum voluptates culpa eaque similique alias.</p>
+    <div class="parallax-container hide-on-med-and-down">
+        <div class="parallax"><img src="/public/assets/img/wave/2-landscape.jpg"></div>
+    </div>
+    <!-- parallax END -->
+    <section class="gray lighten-5">
+        <div class="container">
+            <div class="row">
+                <div class="col s12">
+                    <h3 class="center">News</h3>
+                    <blockquote>
+                        "surf the surfing news."   Mao-Tse Tong
+                    </blockquote>
+                    <p class="flow-text">We made this app to make you the respectfull and informed surfer our ocean needs... <br> no more getting hurt by unespected rocks, no more flat oceans, no more feeling alone.. <br> we are a community to share good moments and help each other to get the waves we need. </p>
+                    <p class="flow-text">We made this app to make you the respectfull and informed surfer our ocean needs... <br> no more getting hurt by unespected rocks, no more flat oceans, no more feeling alone.. <br> we are a community to share good moments and help each other to get the waves we need. </p>
+                    <p class="flow-text">We made this app to make you the respectfull and informed surfer our ocean needs... <br> no more getting hurt by unespected rocks, no more flat oceans, no more feeling alone.. <br> we are a community to share good moments and help each other to get the waves we need. </p>
+                    <p class="flow-text">We made this app to make you the respectfull and informed surfer our ocean needs... <br> no more getting hurt by unespected rocks, no more flat oceans, no more feeling alone.. <br> we are a community to share good moments and help each other to get the waves we need. </p>
+                </div>
             </div>
-        </section>
+        </div>
+    </section>
+    <!-- parallax -->
+    <div class="parallax-container hide-on-large-only">
+        <div class="parallax"><img src="/public/assets/img/wave/3-landscape.jpg"></div>
     </div>
-</section>
-<!-- parallax -->
-<div class="parallax-container hide-on-large-only">
-    <div class="parallax"><img src="/public/assets/img/wave/1-tablet.jpg"></div>
-</div>
-<div class="parallax-container hide-on-med-and-down">
-    <div class="parallax"><img src="/public/assets/img/wave/1-landscape.jpg"></div>
-</div>
-<!-- parallax END -->
-<section class="gray lighten-5">
-    <div class="container">
-        <section class="row">
-            <div class="col s12">
-                <h3 class="center-align ">Contact Us</h3>
-                <blockquote>
-                    conversation is the best way to know each other.
-                </blockquote>
-                <form>
-                    <label for="">name</label>
-                    <input name="" type="text">
-                    <label for="">Email</label>
-                    <input name="" type="text">
-                    <label for="">Cellhpone</label>
-                    <input name="" type="text">
-
-                    <button>send</button>
-                </form>
-            </div>
-        </section>
+    <div class="parallax-container hide-on-med-and-down">
+        <div class="parallax"><img src="/public/assets/img/wave/3-landscape.jpg"></div>
     </div>
-</section>
-<!-- parallax -->
-<div class="parallax-container hide-on-large-only">
-    <div class="parallax"><img src="/public/assets/img/wave/1-tablet.jpg"></div>
-</div>
-<div class="parallax-container hide-on-med-and-down">
-    <div class="parallax"><img src="/public/assets/img/wave/1-landscape.jpg"></div>
-</div>
-<!-- parallax END -->
+    <!-- parallax END -->
+    <section class="gray lighten-5">
+        <div class="container">
+            <section class="row">
+                <div class="col s12">
+                    <h3 class="center-align ">Map</h3>
+                    <blockquote>
+                        This is an example quotation that uses the blockquote tag.
+                    </blockquote>
+                    <p class="flow-text">this is a paragraph inside section,<br> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis distinctio amet veniam odit consequuntur, repudiandae reiciendis est minima et quam optio esse maiores corrupti rerum voluptates culpa eaque similique alias.</p>
+                    <p class="flow-text">this is a paragraph inside section,<br> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis distinctio amet veniam odit consequuntur, repudiandae reiciendis est minima et quam optio esse maiores corrupti rerum voluptates culpa eaque similique alias.</p>
+                    <p class="flow-text">this is a paragraph inside section,<br> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis distinctio amet veniam odit consequuntur, repudiandae reiciendis est minima et quam optio esse maiores corrupti rerum voluptates culpa eaque similique alias.</p>
+                </div>
+            </section>
+        </div>
+    </section>
+    <!-- parallax -->
+    <div class="parallax-container hide-on-large-only">
+        <div class="parallax"><img src="/public/assets/img/wave/4-landscape.jpg"></div>
+    </div>
+    <div class="parallax-container hide-on-med-and-down">
+        <div class="parallax"><img src="/public/assets/img/wave/4-landscape.jpg"></div>
+    </div>
+    <!-- parallax END -->
+    <section class="gray lighten-5">
+        <div class="container">
+            <section class="row">
+                <div class="col s12">
+                    <h3 class="center-align ">Join Us</h3>
+                    <blockquote>
+                        stop beeing a haole, and make a new Surf account...
+                    </blockquote>
+                    <a class="btn waves-effect" href="/user"></a>
+                </div>
+            </section>
+        </div>
+    </section>
+    <!-- parallax -->
+    <div class="parallax-container hide-on-large-only">
+        <div class="parallax"><img src="/public/assets/img/wave/5-landscape.jpg"></div>
+    </div>
+    <div class="parallax-container hide-on-med-and-down">
+        <div class="parallax"><img src="/public/assets/img/wave/5-landscape.jpg"></div>
+    </div>
+    <!-- parallax END -->
 
 </main>
