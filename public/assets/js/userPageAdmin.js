971d8cc3bf21262b33b25dde57e5b7e853b7be3f
ALBANO Daniel <deanidev@gmail.com>
2022-11-28 12:31:47 +0100

    wip - dashboard selective

--- public/assets/js/userPageAdmin.js
+++ public/assets/js/userPageAdmin.js
@@ -1,4 +1,23 @@
+// COLLAPSIBLE INIT 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems);
   });
+
+
+  
+// js for the preview image upload 
+uploadedArticleImg.onchange = () => {
+  const [file] = uploadedArticleImg.files
+  if (file) {
+    showArticleImg.src = URL.createObjectURL(file);
+  }
+}
+
+
+// TABS INIT 
+document.addEventListener('DOMContentLoaded', function() {
+  var el = document.querySelectorAll('.tabs');
+  var instance = M.Tabs.init(el);
+});
+
--- views/userPageAdmin.php
+++ views/userPageAdmin.php
@@ -36,17 +36,21 @@
             <li>
                 <div class="collapsible-header"><i class="material-icons">pageview</i>PAGES</div>
                 <ul class="collapsible-body">
-                    <li><a href="#home"><i class="material-icons">home</i>Accueil</a></li>
-                    <li><a href="#news"><i class="material-icons">assignment_late</i>News</a></li>
-                    <li><a href="#map"><i class="material-icons">map</i>Map</a></li>
+                    <ul class="tabs">
+                        <li class="tab"><a href="#home"><i class="material-icons">home</i>Accueil</a></li>
+                        <li class="tab"><a href="#news"><i class="material-icons">assignment_late</i>News</a></li>
+                        <li class="tab"><a href="#map"><i class="material-icons">map</i>Map</a></li>
+                    </ul>
                 </ul>
             </li>
             <li>
                 <div class="collapsible-header"><i class="material-icons">place</i>Moderation</div>
                 <ul class="collapsible-body">
-                <li><a href="#newsComments"><i class="material-icons">assignment_late</i>News Comments</a></li>
-                <li><a href="#mapComments"><i class="material-icons">assignment_late</i>Map Comments</a></li>
-                <li><a href="#users"><i class="material-icons">assignment_late</i>Users</a></li>
+                    <ul class="tabs">
+                        <li class="tab"><a href="#newsComments"><i class="material-icons">assignment_late</i>News Comments</a></li>
+                        <li class="tab"><a href="#mapComments"><i class="material-icons">assignment_late</i>Map Comments</a></li>
+                        <li class="tab"><a href="#users"><i class="material-icons">assignment_late</i>Users</a></li>
+                    </ul>
                 </ul>
             </li>
         </ul>
@@ -61,4 +65,152 @@
         <li><a class="waves-effect" href="?p=terms">user terms</a></li>
     </ul>
 
+    <!-- NEWS NAV SECTION  -->
+    <section id="news" class="center">
+        <div class="row">
+
+            <nav class="col s12 light-blue darken-2">
+                <h5 class="center">NEWS</h5>
+            </nav>
+            <!-- error/sucess output  -->
+            <?php
+            if (SessionFlash::exist()) {
+                $msg = SessionFlash::get();
+                if ($msg[0] == true) { ?>
+                    <p class='green center white-text'>
+                    <?php
+                } else { ?>
+                    <p class='red center white-text'>
+                    <?php
+                }
+                print_r($msg[1]) ?>
+                    </p>
+                <?php
+            } ?>
+
+
+                <div class="row">
+                    <ul class="tabs">
+                        <li class="tab col s6"><a href="#newsForm">Nouvelle</a></li>
+                        <li class="tab col s6"><a href="#newsList">Liste</a></li>
+                    </ul>
+                </div>
+
+                <!-- NEWS FORM  -->
+                <form id="newsForm" class="container" action="" method="post" enctype="multipart/form-data">
+                    <!-- header  -->
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <input id="input_text" type="text" data-length="30">
+                            <label for="input_text">Main title</label>
+                        </div>
+                    </div>
+                    <!-- headlines / sub-title -->
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <input id="input_text" type="text" data-length="100">
+                            <label for="input_text">headline - blockquote</label>
+                        </div>
+                    </div>
+                    <!-- main image  -->
+                    <div class="row">
+                        <!-- image placeholder -->
+                        <div class="col s12">
+                            <!-- look for CSS  -->
+                            <img id="showArticleImg" class="responsive-img" src="/public/assets/img/default-image.jpg" alt="article image" />
+                        </div>
+                    </div>
+                    <!--  image input  -->
+                    <div class="row">
+                        <div class="file-field input-field col s12">
+                            <span>choisir article image</span>
+                            <input name="articleimage" type="file" accept="image/png, image/jpeg" id="uploadedArticleImg" placeholder="choose article image">
+                        </div>
+                    </div>
+                    <!-- text  -->
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <textarea id="textarea1" class="materialize-textarea"></textarea>
+                            <label for="textarea1">ecrire article:</label>
+                        </div>
+                    </div>
+                    <!-- submit -->
+                    <div class="row center-align">
+                        <button class="unset"><a class="waves-effect waves-light btn">create</a></button>
+                    </div>
+                </form>
+
+                <!-- NEWS LIST  -->
+                <div id="newsList" class="container center">
+                    <ul class="row collapsible">
+                        <li>
+                            <div class="collapsible-header">Shark's bite for love <span class="grey-text">-20 dec 2020</span></div>
+                            <div class="collapsible-body">
+                                <div>image thumb</div>
+                                <div>headlines</div>
+                                <div>content</div>
+                                <div class="btns row">
+                                    <span class="btn col s4">comments</span>
+                                    <span class="btn col s4">edit</span>
+                                    <span class="btn col s4">delete</span>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div class="collapsible-header">Mondial surf hawaii <span class="grey-text">-20 dec 2020</span></div>
+                            <div class="collapsible-body">
+                                <div>image thumb</div>
+                                <div>headlines</div>
+                                <div>content</div>
+                                <div class="btns row">
+                                    <span class="btn col s4">comments</span>
+                                    <span class="btn col s4">edit</span>
+                                    <span class="btn col s4">delete</span>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="collapsible-header">Learn well by eating well <span class="grey-text"> le 20 dec 2020</span></div>
+                            <div class="collapsible-body">
+                                <div>image thumb</div>
+                                <div>headlines</div>
+                                <div>content</div>
+                                <div class="btns row">
+                                    <span class="btn col s4">comments</span>
+                                    <span class="btn col s4">edit</span>
+                                    <span class="btn col s4">delete</span>
+                                </div>
+                            </div>
+                        </li>
+
+                    </ul>
+                </div>
+        </div>
+
+    </section>
+
+    <!-- MAP NAV SECTION   -->
+    <section id="map" class="center">
+        <div class="row">
+
+            <nav class="col s12 grey darken-1">
+                <h5 class="center">MAP</h5>
+            </nav>
+            <!-- error/sucess output  -->
+            <?php
+            if (SessionFlash::exist()) {
+                $msg = SessionFlash::get();
+                if ($msg[0] == true) { ?>
+                    <p class='green center white-text'>
+                    <?php
+                } else { ?>
+                    <p class='red center white-text'>
+                    <?php
+                }
+                print_r($msg[1]) ?>
+                    </p>
+                <?php
+            } ?>
+    </section>
 </main>
