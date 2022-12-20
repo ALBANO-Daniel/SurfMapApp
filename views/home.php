6ebce671a45a0cf7160d609098de0f16aeb85940
ALBANO-Daniel <deanidev@gmail.com>
2022-12-20 02:33:16 +0100

    translate home done

--- views/home.php
+++ views/home.php
@@ -3,12 +3,12 @@
     <!-- parallax -->
     <div class="row s12">
         <div class="parallax-container valign-wrapper hide-on-large-only">
-            <h2 class="grey-text text-lighten-5">Meet<br>the waves<br>of your dreams!</h2>
+            <h2 class="grey-text text-lighten-5">Rencontrer<br>les vagues<br>des vos rêves!</h2>
             <div class="parallax"><img src="/public/assets/img/wave/1-tablet.jpg"></div>
         </div>
 
         <div class="parallax-container valign-wrapper hide-on-med-and-down">
-            <h2 class="grey-text text-lighten-5">Meet<br>the waves<br>of your dreams!</h2>
+            <h2 class="grey-text text-lighten-5">Rencontrer<br>les vagues<br>des vos rêves!</h2>
             <div class="parallax"><img src="/public/assets/img/wave/1-landscape.jpg"></div>
         </div>
     </div>
@@ -20,13 +20,13 @@
             <div class="row">
                 <div class="col s12">
                     <div class="card text-card">
-                        <h3 class="center-align ">where are the haoles !?</h3>
+                        <h3 class="center-align ">où sont les haoles !?</h3>
                         <blockquote>
-                            "If you're having a bad day, catch a wave."
+                            "Si vous passez une mauvaise journée, attrapez une vague!"
                         </blockquote>
-                        <p class="flow-text">We made this app to help you be respectfull and informed surfer our ocean needs... no more getting hurt by unespected rocks, no more flat oceans, no more feeling alone.. we are a community to share good moments and help each other get the waves we need! </p>
-                        <p class="flow-text">Experience to be part of a community, help each other out sharing information and reacting to the most recent news in the world of Surf and be the first to know about whats happening.</p>
-                        <p class="flow-text">Having access to information about the surf news and community progress to make surf better and safer for everyone, have also access to a lot of information about the best spots around you, or when you travel, giving the oportunity to help you know news places and surf safe as if you were a local.</p>
+                        <p class="flow-text">Nous avons créé cette application pour vous aider à être un surfeur respectueux et informé dont nos océans ont besoin... plus de blessures par des rochers insoupçonnés, plus d'océans plats, plus de sentiment de solitude... nous sommes une communauté pour partager de bons moments et nous entraider les vagues dont nous avons besoin !</p>
+                        <p class="flow-text">Faites l'expérience de faire partie d'une communauté, aidez-vous les uns les autres à partager des informations et à réagir aux dernières nouvelles du monde du surf et soyez les premiers informés de ce qui se passe.</p>
+                        <p class="flow-text">Avoir accès à des informations sur l'actualité du surf et les progrès de la communauté pour rendre le surf meilleur et plus sûr pour tout le monde, avoir également accès à de nombreuses informations sur les meilleurs spots autour de vous, ou lorsque vous voyagez, ce qui vous donne l'opportunité de vous aider à connaître les nouveaux lieux et surfez en toute sécurité comme si vous étiez un local.</p>
                     </div>
                 </div>
             </div>
@@ -47,11 +47,11 @@
                     <div class="card text-card">
                         <h3 class="center">News</h3>
                         <blockquote class="left-align">
-                            "surf the surfing news." - Maot et ce Tongue
+                            "surfez sur l'actualité du surf." - Maot et ce Tongue
                         </blockquote>
-                        <p class="flow-text">Our news section helps you keep up with all that is happening in the surf universe, competitions, gatherings, parties, and new iniciatives to help the enviroment of the surfers, to improve the sport and all the things related to a good surf experience and culture spreading. helping everyone to keep up with all that is new and important on the surf arround the world! </p>
+                        <p class="flow-text">Notre section d'actualités vous aide à suivre tout ce qui se passe dans l'univers du surf, les compétitions, les rassemblements, les fêtes et les nouvelles initiatives pour aider l'environnement des surfeurs, améliorer le sport et tout ce qui est lié à une bonne expérience et culture du surf diffusion. aider tout le monde à se tenir au courant de tout ce qui est nouveau et important sur le surf dans le monde !</p>
                         <br>
-                        <a class="btn waves-effect" href="/news">see news</a>
+                        <a class="btn waves-effect" href="/news">actualités!</a>
                         <br>
                         <br>
                     </div>
@@ -74,13 +74,13 @@
                     <div class="card text-card">
                         <h3 class="center-align">Map</h3>
                         <blockquote>
-                            This is an example quotation that uses the blockquote tag.
+                        une carte est une carte, pour un pirate ou un guerrier, le trésor est le même
                         </blockquote>
-                        <p class="flow-text">This is our main feature, we offer access to the best spots of surf for you to know and visit, using the precision of GPS location, and information from the best surfers in the country to let you know how the wave work, all the information and details of the spot, so you can have a fun and be safe out there. </p>
+                        <p class="flow-text">C'est la principale caractéristique de notre site Web, nous vous offrons l'accès aux meilleurs spots de surf à connaître et à visiter, en utilisant la précision de la localisation GPS, et les informations des meilleurs surfeurs du pays pour vous faire savoir comment fonctionne la vague, toutes les informations et les détails du spot, pour que vous puissiez vous amuser et être en sécurité là-bas.</p>
                         <div class="center">
                             <img class="center-align" width="70%" height="50%" src="/public/assets/img/map-example.jpg" alt="example map">
                         </div>
-                        <a class="btn waves-effect" href="/map">check it out!</a>
+                        <a class="btn waves-effect" href="/map">trésor!</a>
                         <br>
                         <br>
                     </div>
@@ -105,13 +105,13 @@
                  
                         <h3 class="center-align">Join Us</h3>
                         <blockquote class="left-align">
-                            stop beeing a haole, and make a new Surf account...
+                            arrête d'être un haole, et crée un nouveau compte...
                         </blockquote>
 
                         <br>
-                        <p class="flow-text">creating an account only take 1 minute! you don't need to share any sensible personal information, and you get to be part of our community and will enjoy the benefits of beeing a member!</p>
+                        <p class="flow-text">créer un compte ne prend qu'1 minute ! vous n'avez pas besoin de partager des informations personnelles sensibles, et vous faites partie de notre communauté et profiterez des avantages d'être membre !</p>
                         <br>
-                        <a class="btn waves-effect" href="/user">join us</a>
+                        <a class="btn waves-effect" href="/user">Rejoignez-nous</a>
                         <br>
                         <br>
                     
@@ -127,5 +127,4 @@
         <div class="parallax"><img src="/public/assets/img/wave/5-landscape.jpg"></div>
     </div>
     <!-- parallax END -->
-<a href="/admin">go to admin</a>
 </main>
